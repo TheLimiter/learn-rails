5f7d22089e1bb48f6669439aa8d51f88944b4d97
rizkyramon <rizkyramon1@gmail.com>
2020-02-06 14:12:26 +0700

    ifpertama konversimenit perbandingan

---
+++ perbandingan.rb
@@ -0,0 +1,19 @@
+uts = "PROGRAM PEMBAGIAN BILANGAN"
+
+print "Masukan a: "
+
+a = gets.to_f
+
+print "Masukan b: "
+
+b = gets.to_f
+
+c = a/b
+
+if a/b
+
+puts "#{c}"
+
+puts "#{a} / #{b} = #{c}"
+
+end
