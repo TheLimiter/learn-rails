812b988214807039eb7022e64e078856e4603347
rezhap <rezha7890@gmail.com>
2020-02-06 14:44:44 +0700

    New member

--- bakuhantam.rb
+++ bakuhantam.rb
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-
-rifqi irianto
-
-
-wowkowkwoKWOKWOWKOWKWOWOK
---faiz :v
-ofiiiiii
-
-=======
 faiz
 rifqi irianto
 dandung
-
-
-
+arjuna efenus
 ofiiiiiiiiiii
->>>>>>> 440eab9aacd42b40cc6bbccb218d9f99be8fb3f8
