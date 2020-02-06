6fa8c8f1d6e33f359978f233fd1c2cb1e6046281
dandungjn <dandungjnjnjn@gmail.com>
2020-02-06 14:07:10 +0700

    Tes blind code - Dandung

---
+++ ifpertama.rb
@@ -0,0 +1,11 @@
+puts "PROGRAM PEMBAGIAN BILANGAN"
+print "masukan a: "
+a = gets.to_f
+print "masukan b: "
+b = gets.to_f
+if a/b
+c = a/b
+puts "#{c}"
+else
+puts "#{a} / #{b} = #{c}"
+end
---
+++ perbandingan.rb
@@ -0,0 +1,15 @@
+def bandingkanAngka(number1,number2)
+if number1 == number2
+print "-1"
+elsif number1 < number2
+print true
+else
+print false
+end
+end
+
+puts bandingkanAngka(5,8)
+puts bandingkanAngka(5,3)
+puts bandingkanAngka(4,4)
+puts bandingkanAngka(3,3)
+puts bandingkanAngka(17,2)
