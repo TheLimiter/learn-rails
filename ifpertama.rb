74e1a4da98d84ac3917a40402debfcec7431fbe3
Akbar Fadilah <muhamadakbarfadilah6@gmail.com>
2020-02-06 14:12:36 +0700

    AkbarFdlh

---
+++ ifpertama.rb
@@ -0,0 +1,20 @@
+puts = "PROGRAM PEMBAGIAN BILANGAN"
+
+print "Masukan a: "
+
+a = gets.to_f
+
+print "Masukan b: "
+
+b = gets.to_f
+
+c = a/b
+
+if a/b
+
+puts "#{c}"
+
+puts "#{a} / #{b} = #{c}"
+
+end
+
