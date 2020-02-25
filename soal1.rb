3cb05d7f176086592af63c297b2f612ca2833f62
akbarfdlh2 <muhamadakbarfadilah6@gmail.com>
2020-02-25 14:05:05 +0700

    Exam1

---
+++ soal1.rb
@@ -0,0 +1,10 @@
+#No 1
+puts "===No 1==="
+lemari = ["Buku","Handphone","Pensil","Baju","Flashdrive"]
+tas = []
+    tas << lemari.delete_at(1)
+    tas << lemari.delete_at(2)
+puts "+++Lemari+++"
+    puts lemari
+puts "+++Tas+++"
+    puts tas
---
+++ soal2.rb
@@ -0,0 +1,9 @@
+#No 2
+puts "===No 2==="
+array1 = [1,2,3,4,5,6,7,8,9]
+array2 = [1,7,6,0]
+
+puts "+++Array 1+++"
+    puts "#{array1.rotate{6}}"
+puts "+++Array 2+++"
+    puts "#{array2.rotate{1}}"
