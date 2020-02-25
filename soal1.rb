5b8c5772e21b48b7c7142445f51968a851755fe7
Tabah <yusronre@gmail.com>
2020-02-25 14:03:43 +0700

    Exam1

---
+++ soal1.rb
@@ -0,0 +1,10 @@
+lemari = ["buku","handphone","pensil","baju","flashdrive"]
+tas = []
+tas << lemari.delete_at(2)
+tas << lemari.delete_at(0)
+tas << lemari.delete_at(2)
+puts"====lemari===="
+puts lemari
+puts "===========tas==========="
+puts tas
+puts "==============================================================="
---
+++ soal3.rb
@@ -0,0 +1,15 @@
+buah = {apel:5000,nanas:10000,semangka:20000,jeruk:4000,anggur:6000}
+totalHarga = 0
+for list in buah
+    puts list
+    jumlah = gets.to_i
+    harga = list[1] * jumlah
+    totalHarga = totalHarga + harga
+end 
+if totalHarga >= 50000
+    diskon = totalHarga * 10/100
+    hargaDiskon = totalHarga - diskon
+    puts "Total harga anda adalah #{hargaDiskon}"
+else
+    puts "Total harga anda adalah #{totalHarga}"
+end
