1b978e3e9bd2ae4accae55fcd3f98eac6484b916
mfaiza <mfaizaschool@gmail.com>
2020-02-25 13:42:53 +0700

    Exam-1 Faiz

---
+++ exam1.rb
@@ -0,0 +1,13 @@
+lemari = ["Buku","Handphone","Pensil","Baju","Flashdrive"]
+tas = []
+print "\n========TAS =======\n"
+tas << lemari.delete_at(0)
+tas << lemari.delete_at(1)
+tas << lemari.delete_at(2)
+
+puts tas
+
+print"========== Lemari ==========\n"
+
+puts lemari
+# puts lemari
---
+++ exam2.rb
@@ -0,0 +1,6 @@
+array1 = [1,2,3,4,5,6,7,8,9]
+array2 = [1,7,6,0]
+
+print "#{array1.rotate(-3)}\n" 
+puts "==============="
+print "#{array2.rotate(-3)}"
