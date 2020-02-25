30e1cea82e0f1cdedb266f3890a6e98769f5ab93
dandungjn <dandungjnjnjn@gmail.com>
2020-02-25 14:00:33 +0700

    exam1

---
+++ soal1.rb
@@ -0,0 +1,9 @@
+lemari = ["buku","handphone","pensil","baju","flashdrive"]
+tas = []
+tas << lemari.delete_at(2)
+tas << lemari.delete_at(0)
+tas << lemari.delete_at(2)
+puts"=========lemari==========="
+puts lemari
+puts "===========tas==========="
+puts tas
---
+++ soal2.rb
@@ -0,0 +1,6 @@
+
+array1 = [1,2,3,4,5,6,7,8,9]
+array2 = [1,7,6,0]
+
+puts "#{array1.rotate!(6)}"
+puts "#{array2.rotate!(1)}"
