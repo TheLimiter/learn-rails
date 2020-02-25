7b82361384ae8b0590f0b8e4e61ca539a31f8fe8
MuhammadYaumil1212 <54166418+MuhammadYaumil1212@users.noreply.github.com>
2020-02-25 14:21:03 +0700

    update soal3.rb
    Dikarenakan salah file

--- soal3.rb
+++ soal3.rb
@@ -1,5 +1,29 @@
 #hash , jika pembelian > 50000 dapet diskon 10%
-buah = {apel:5000,nanas:10000,semangka:70000,jeruk:4000,anggur:6000}
-buah.each do |value, key|
-    puts "Buah : #{value} (#{key})"
+class Transaksi
+    buah = {apel:5000,nanas:10000,semangka:70000,jeruk:4000,anggur:6000}
+    buah.each do |value, key|
+        puts "Buah : #{value} (#{key})"
+    end
+def Question
+    harga = 100000
+puts ""
+puts "Beli Apa?"
+jawaban = gets.chomp 
+      if jawaban == jawaban
+        puts "Berapa Biji?"
+        jawaban2 = gets.to_i
+      end
+     total = jawaban2*harga
+     diskon = total * 10/100
+     totalDiskon = total - diskon
+     if total > 50000
+        puts "Total dengan Diskon #{totalDiskon}"
+     else
+        puts "total harga : #{total}"
+     end
 end
+end 
+
+buah = Transaksi.new
+buah.Question()
+
