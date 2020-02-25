0d121c23eab7f69f42f13b8255d7b5725fbe8eed
rezhap <rezha7890@gmail.com>
2020-02-25 14:06:00 +0700

    Exam1

---
+++ exam1.rb
@@ -0,0 +1,12 @@
+
+lemari = ["Buku","Handphone","Pensil","Baju","Flashdrive"]
+tas = []
+
+tas << lemari.delete_at(1)
+tas << lemari.delete_at(2)
+
+puts lemari
+puts '======================'
+puts tas
+
+
